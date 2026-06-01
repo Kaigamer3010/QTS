1daa4dc8c36d19951eea39b352c28cf73081af3e
kaigamer3010 <isaquean301007@gmail.com>
2026-05-31 22:35:46 -0300

    implementação dos exercícios

--- script.js
+++ script.js
@@ -6,4 +6,144 @@ function mensagemBoasVindas(nome) {
   return `Olá, ${nome}!`;
 }
 
-module.exports = { somar, mensagemBoasVindas };
+//ex01
+function valorMaior(a, b) {
+  if (typeof a == "number" && typeof b == "number") {
+    if (a == b) {
+      return "são iguais";
+    }
+    if (a > b) {
+      return a;
+    } return b;
+  } return "erro";
+
+}
+
+
+//ex02
+function parImpar(a, b) {
+  if (typeof a == "number" && typeof b == "number") {
+    let texto = "";
+    if (a % 2 == 0) {
+      texto += `${a} é par e `;
+    } else { texto += `${a} é ímpar e `; }
+    if (b % 2 == 0) {
+      texto += `${b} é par`;
+    } else { texto += `${b} é ímpar`; }
+    return texto;
+  } return "Erro";
+}
+
+
+//ex03
+function votar(n) {
+  if (n < 0 || typeof n != "number") {
+    return "erro"
+  }
+  if (n < 16) {
+    return "Não pode votar"
+  }
+  if (18 <= n && n < 60) {
+    return "Deve votar";
+  } else {
+    return "Pode votar"
+  }
+}
+
+//ex04
+function positivo(n) {
+  if (typeof n == "number") {
+    if (n < 0) {
+      return "Negativo"
+    }
+    if (n == 0) {
+      return "Zero";
+    } return "Positivo"
+  } return "erro"
+}
+
+//ex05
+function bonus(n) {
+  if (n < 0 || typeof n != "number") {
+    return "erro"
+  }
+  if (n < 2000) {
+    return ("bônus é de: " + n * 0.2 + "R$")
+  }
+  if (n < 5000) {
+    return ("bônus é de: " + n * 0.1 + "R$")
+  } else {
+    return ("bônus é de: " + n * 0.05 + "R$")
+  }
+}
+
+//ex06
+function dirigir(n) {
+  if (n < 0 || typeof n != "number") {
+    return "erro"
+  }
+  if (n < 18) {
+    return "Não pode dirigir"
+  } else {
+    return "Pode dirigir"
+  }
+}
+
+//ex07
+function semestre(n) {
+  if (n < 0 || n > 12 || typeof n != "number") {
+    return "erro"
+  }
+  if (n < 7) {
+    return "1Semestre"
+  } return "2Semestre"
+}
+
+//ex08
+function vogal(s) {
+  const alfabetoGeral = [
+    'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
+    'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'
+  ];
+
+  const vogal = ["A", "a", "E", "e", "I", "i", "O", "o", "U", "u"]
+
+  if (alfabetoGeral.includes(s)) {
+    if (vogal.includes(s)) {
+      return "É vogal";
+    } return "É consoante";
+  } return "erro";
+}
+
+//ex09
+function media(a, b, c, d, e) {
+  let vetor = [a, b, c, d, e];
+
+  for (let i = 0; i < vetor.length; i++) {
+    if(typeof vetor[i] != "number") {
+      return "erro";
+    }  
+  }
+  let media = 0;
+
+  for (let i = 0; i < vetor.length; i++) {
+    media += vetor[i]
+  }
+
+  return media / 5;
+}
+
+
+//ex10
+function cadastro(u, s) {
+  let user = "Admin";
+  let senha = "123";
+
+  if (u == user && s == senha) {
+    return "Conectado com sucesso!"
+  } else { return "Acesso negado! Usuário ou senha incorretos!" }
+}
+
+
+
+module.exports = { somar, mensagemBoasVindas, valorMaior, parImpar, votar, positivo, bonus, dirigir, semestre, vogal, media, cadastro };
--- script.test.js
+++ script.test.js
@@ -1,4 +1,4 @@
-const { somar, mensagemBoasVindas } = require('./script');
+const { somar, mensagemBoasVindas, valorMaior, parImpar, votar, positivo, bonus, dirigir, semestre, vogal, media, cadastro } = require('./script');
 
 test('soma dois números', () => {
   expect(somar(2, 3)).toBe(5);
@@ -6,5 +6,175 @@ test('soma dois números', () => {
 
 test('mensagem de boas vindas', () => {
   expect(mensagemBoasVindas('Diogo')).toBe('Olá, Diogo!');
-  
+
+
+//ex01
+});
+test('Qual é o maior', () => {
+  expect(valorMaior(1, 2)).toBe(2);
+});
+test('Qual é o maior', () => {
+  expect(valorMaior(1, -2)).toBe(1);
+});
+test('Qual é o maior', () => {
+  expect(valorMaior("1", -2)).toBe("erro");
+});
+test('Qual é o maior', () => {
+  expect(valorMaior(1, "asdf")).toBe("erro");
+});
+test('Qual é o maior', () => {
+  expect(valorMaior(1, 1)).toBe("são iguais");
+});
+
+
+//ex02
+
+test("se os números são pares ou impares", () => {
+  expect(parImpar(1, 3)).toBe("1 é ímpar e 3 é ímpar");
+})
+test("se os números são pares ou impares", () => {
+  expect(parImpar(1, 222)).toBe("1 é ímpar e 222 é par");
+})
+test("se os números são pares ou impares", () => {
+  expect(parImpar(-2, 124)).toBe("-2 é par e 124 é par");
+})
+test("se os números são pares ou impares", () => {
+  expect(parImpar(-2, "124")).toBe("Erro");
+})
+
+
+//ex03
+
+test("se tem idade para votar", () => {
+  expect(votar("asf")).toBe("erro");
+})
+test("se tem idade para votar", () => {
+  expect(votar(-2)).toBe("erro");
+})
+test("se tem idade para votar", () => {
+  expect(votar(3)).toBe("Não pode votar");
+})
+test("se tem idade para votar", () => {
+  expect(votar(17)).toBe("Pode votar");
+})
+test("se tem idade para votar", () => {
+  expect(votar(63)).toBe("Pode votar");
+})
+test("se tem idade para votar", () => {
+  expect(votar(43)).toBe("Deve votar");
+})
+
+//ex04
+
+test("se o numero é postivo", () => {
+  expect(positivo("sadfas")).toBe("erro");
+});
+test("se o numero é postivo", () => {
+  expect(positivo(1243)).toBe("Positivo");
+});
+test("se o numero é postivo", () => {
+  expect(positivo(-4)).toBe("Negativo");
+});
+test("se o numero é postivo", () => {
+  expect(positivo(0)).toBe("Zero");
+});
+
+//ex05
+
+test("se o bonus esta sendo calculado", () => {
+  expect(bonus("sadfas")).toBe("erro");
+});
+test("se o bonus esta sendo calculado", () => {
+  expect(bonus(-4)).toBe("erro");
+});
+test("se o bonus esta sendo calculado", () => {
+  expect(bonus(1500)).toBe("bônus é de: 300R$");
+});
+test("se o bonus esta sendo calculado", () => {
+  expect(bonus(3000)).toBe("bônus é de: 300R$");
+});
+test("se o bonus esta sendo calculado", () => {
+  expect(bonus(10000)).toBe("bônus é de: 500R$");
+});
+
+//ex06
+
+test("se tem idade para dirigir", () => {
+  expect(dirigir("sadfas")).toBe("erro");
+});
+test("se tem idade para dirigir", () => {
+  expect(dirigir(-4)).toBe("erro");
+});
+test("se tem idade para dirigir", () => {
+  expect(dirigir(150)).toBe("Pode dirigir");
+});
+test("se tem idade para dirigir", () => {
+  expect(dirigir(3)).toBe("Não pode dirigir");
+});
+
+//ex07
+
+test("qual semestre o mes pertence", () => {
+  expect(semestre("sadfas")).toBe("erro");
+});
+test("qual semestre o mes pertence", () => {
+  expect(semestre(-4)).toBe("erro");
+});
+test("qual semestre o mes pertence", () => {
+  expect(semestre(150)).toBe("erro");
+});
+test("qual semestre o mes pertence", () => {
+  expect(semestre(3)).toBe("1Semestre");
+});
+test("qual semestre o mes pertence", () => {
+  expect(semestre(7)).toBe("2Semestre");
+});
+
+//ex08
+
+test("se a letra é vogal ou consoante", () => {
+  expect(vogal("sadfas")).toBe("erro");
+});
+test("se a letra é vogal ou consoante", () => {
+  expect(vogal(1234)).toBe("erro");
+});
+test("se a letra é vogal ou consoante", () => {
+  expect(vogal("a")).toBe("É vogal");
+});
+test("se a letra é vogal ou consoante", () => {
+  expect(vogal("s")).toBe("É consoante");
+});
+
+//ex09
+
+test("qual a media aritmetica dos 5 numeros", () => {
+  expect(media("sadfas", 2, 3, 4, 5)).toBe("erro");
+});
+test("qual a media aritmetica dos 5 numeros", () => {
+  expect(media(1, 3, 5, 7, 9)).toBe(5);
+});
+test("qual a media aritmetica dos 5 numeros", () => {
+  expect(media(1, 5, 7, 23, 54)).toBe(18);
+});
+
+
+//ex10
+
+test("se a senha e o usuario estão corretos", () => {
+  expect(cadastro(1, 2)).toBe("Acesso negado! Usuário ou senha incorretos!");
+});
+test("se a senha e o usuario estão corretos", () => {
+  expect(cadastro("Admin", "afasdf")).toBe("Acesso negado! Usuário ou senha incorretos!");
+});
+test("se a senha e o usuario estão corretos", () => {
+  expect(cadastro("Admin", "123")).toBe("Conectado com sucesso!");
+});
+test("se a senha e o usuario estão corretos", () => {
+  expect(cadastro("Admin", " 123 ")).toBe("Acesso negado! Usuário ou senha incorretos!");
+});
+test("se a senha e o usuario estão corretos", () => {
+  expect(cadastro("asdf", "123")).toBe("Acesso negado! Usuário ou senha incorretos!");
+});
+test("se a senha e o usuario estão corretos", () => {
+  expect(cadastro("", "")).toBe("Acesso negado! Usuário ou senha incorretos!");
 });
